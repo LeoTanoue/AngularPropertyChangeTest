5fb5279dc860ed2ec3b3e9af3db47b3c8eaf0629
Leonardo Tanoue <leo.tanoue@gmail.com>
2017-12-14 13:13:29 -0600

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,12 +13,13 @@ import { FormControl, FormBuilder, FormGroup, Validators } from '@angular/forms'
 export class AppComponent implements OnInit {
   public account: Account;
   public form: FormGroup;
+  public accountId: string;
   constructor(private http: Http, private formBuilder: FormBuilder) {
     this.form = this.formBuilder.group({
-      AccountID: new FormControl(),
-      Name: new FormControl(),
-      DBA: new FormControl(),
-      BusinessStartDate: new FormControl()
+      accountID: new FormControl(),
+      name: new FormControl(),
+      dba: new FormControl(),
+      businessStartDate: new FormControl()
      }
   );
   }
@@ -26,25 +27,21 @@ export class AppComponent implements OnInit {
     const restUrl = 'http://localhost:2756/api/Account/AccountByOppID/94211BC6-90E1-E611-80E0-00155D01E500';
     this.http.get(restUrl, { withCredentials: true }).map((res: Response) => res.json()).subscribe(
       data => {
-        this.account = Object.assign(new Account(), data) as Account;
-        // this.account =  data as Account;
+        this.accountId = data.AccountID;
         this.form.setValue({
-          AccountID: [this.account.AccountID],
-          Name: [this.account.Name],
-          DBA: [this.account.DBA],
-          BusinessStartDate: [this.account.BusinessStartDate]
+          accountID: [data.AccountID],
+          name: [data.Name],
+          dba: [data.DBA],
+          businessStartDate: [data.BusinessStartDate]
         });
       } ,
-      error => console.log(error),
-      () => {
-        console.log(this.account);
-      }
+      error => console.log(error)
     );
   }
 
   onSubmit() {
     if (this.form.dirty) {
-     const updateAccount = new Account(this.account.AccountID);
+     const updateAccount = new Account(this.accountId);
       Object.keys(this.form.controls).forEach(controlName => {
         const currentControl = this.form.controls[controlName];
         if (currentControl.dirty) {
